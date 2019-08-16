d9cd25a3e258236e7d63a8b7bcc169aad648c080
strix <p.smagin@napoleonit.ru>
2019-08-16 19:02:42 +0500

    basic web interface of a calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, web};
+use tera::{Tera, Context};
+use serde::Deserialize;
+use reverse;
+
+
+struct AppState {
+    template: tera::Tera
+}
+
+#[derive(Deserialize)]
+struct CalculatorForm {
+    expression: String
+}
+
+fn index_get(data: web::Data<AppState>) -> web::HttpResponse {
+    let mut context = Context::new();
+    context.insert("input_value", "");
+    web::HttpResponse::Ok()
+        .content_type("text/html")
+        .body(data.template.render("index.html", context).unwrap())
+}
+
+fn index_post(data: web::Data<AppState>, post_data: web::Form<CalculatorForm>) -> web::HttpResponse {
+    let mut context = Context::new();
+    let result = match reverse::eval(&post_data.expression) {
+        Ok(n) => n.to_string(),
+        Err(e) => e
+    };
+    context.insert("result", &result);
+    context.insert("input_value", &post_data.expression);
+    web::HttpResponse::Ok()
+        .content_type("text/html")
+        .body(data.template.render("index.html", context).unwrap())
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .data(AppState {template: Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap()})
+            .service(
+                web::scope("/")
+                    .route("", web::get().to(index_get))
+                    .route("", web::post().to(index_post))
+            )
+    })
+        .bind("127.0.0.1:8088")?
+        .run()
 }
